5d8217d8a111660e2f889407376d95b9ec4149da
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-09 17:22:02 +0500

    minor issue

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ import { deleteAllMessages } from './controllers/deleteChatController.js';
 import { getMessageCount } from './controllers/msgCountController.js';
 import { chatbotController } from './controllers/chatbot.js';
 import authRoutes from "./routes/authRoutes.js"
+import setupPassport from "./passportSetup.js"
 dotenv.config();
 
 const app = express();
@@ -68,6 +69,9 @@ app.post('/chatbot-resp', chatbotController)
 app.post('/jwtverify', jwtVerifyController);
 app.post('/adminlogin', adminLoginController)
 // Login Route
+
+// Setup passport strategies
+setupPassport(passport);
 
 app.use("/", authRoutes)
 // app.get("/googlelogin", passport.authenticate("google", { scope: ["profile", "email"] }));
