a7a79e60b84a32ce7b2803f6867121bf41bb2b50
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-09 17:09:42 +0500

    Back to previous code

--- socket/index.js
+++
@@ -1,9 +0,0 @@
-import { Server } from 'socket.io';
-import setupSocketHandlers from "./handlers/handler.js"
-export default function setupSocket(server) {
-    const io = new Server(server, {
-        cors: { origin: '*', methods: ['GET', 'POST'] }
-    });
-
-    setupSocketHandlers(io);
-}
