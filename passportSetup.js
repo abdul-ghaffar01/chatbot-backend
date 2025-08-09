749314eb42feb5af42c7b5da4557a657ecdbe0b2
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-09 17:23:34 +0500

    Small issue

--- passportSetup.js
+++ passportSetup.js
@@ -1,20 +1,17 @@
-import passport from "passport"
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
-// Passport Google Strategy
-passport.use(
+export default function setupPassport(passport) {
+  passport.use(
     new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.CHATBOT_BACKEND_URL}/auth/google/callback`,
-        },
-        (accessToken, refreshToken, profile, done) => {
-            // Return user profile
-            return done(null, profile);
-        }
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${process.env.CHATBOT_BACKEND_URL}/auth/google/callback`,
+      },
+      (accessToken, refreshToken, profile, done) => done(null, profile)
     )
-);
+  );
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
+}
