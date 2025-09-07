86850c4e57b6863c3ab6b8fcbbbe2323a11d5d19
Abdul Ghaffar <agscontact777@gmail.com>
2025-09-07 13:13:19 +0500

    Resolved some issues

--- utils/generate_jwt.js
+++ utils/generate_jwt.js
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken';
 
 const payload = {};  // empty payload
 
-const token = jwt.sign(payload, process.env.JWT_SECRET_BOT);
+const token = jwt.sign(payload, process.env.JWT_SECRET_FOR_BOT_TO_HIT_RESPONSE_URL);
 
 console.log("Generated Token:", token);
